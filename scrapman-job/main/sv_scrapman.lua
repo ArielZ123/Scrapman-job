57187d8969afc24ffd846db4ddecfa20a12fe22e
ArielZ <74502022+ArielZ123@users.noreply.github.com>
2022-04-19 14:52:30 +0300

    Add files via upload

---
+++ scrapman-job/main/sv_scrapman.lua
@@ -0,0 +1,37 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('scrapjob:scrap:find')
+AddEventHandler('scrapjob:scrap:find', function()
+   local _source = source
+   local xPlayer = ESX.GetPlayerFromId(source)
+   xPlayer.addInventoryItem('scrap', 1)
+end)
+
+RegisterServerEvent('scrapjob:scrap:sell')
+AddEventHandler('scrapjob:scrap:sell', function()
+   local _source = source
+   local xPlayer = ESX.GetPlayerFromId(source)
+   local scrapQuantity = xPlayer.getInventoryItem('scrap').count
+   local addmoney = math.random(20, 50) -- change here the price of the scrap sell
+   if scrapQuantity >= 1 then
+       xPlayer.removeInventoryItem('scrap', 1)
+       xPlayer.addMoney(addmoney)
+       TriggerClientEvent("pNotify:SendNotification", source, {
+          text = "you sold a scrap type for <b style=color:#1588d4>"  .. addmoney .. " <b style=color:#d1d1d1> keep working</b>",
+          type = "success",
+          queue = "lmao",
+          timeout = 7000,
+          layout = "Centerleft"
+       })
+   elseif scrapQuantity then
+      	TriggerClientEvent("pNotify:SendNotification", source, {
+          text = "you dont have any scrap type",
+          type = "success",
+          queue = "lmao",
+          timeout = 7000,
+          layout = "Centerleft"
+        })
+   end
+end)
---
+++ scrapman-job/readme.md
@@ -0,0 +1,16 @@
+Hi there, Thank you for download our cool job here you can get all the information about the job.
+
+About the job: I posted this job a long time ago -> https://forum.cfx.re/t/esx-scrapman-job/3807213/42 
+But unfortunately i had some problems there, and after a while i decided to post the job again with fixes for all the problems that i had there.
+If you find new problems or old ones keep in touch with me in the fivem forums or take my discord name from my github profile link -> https://github.com/ArielZ123
+I will be happy to help or get from you tips to fix things or change.
+
+Settings in the script: There are 3 things you can add or remove from the client side in lines -> 15, 21, 25, all of them are coords for add or remove positions of markers to search or sell and also add the npc Character
+that using as a 'dealer'.
+
+Server side change: In Line 17 you can change the price of the scrap sell -> local addmoney = math.random(20, 50) -> please pay attention to change the numbers to (High or low amount) as your choice.
+
+DO NOT FORGET TO CONNECT THE SQL FILE TO YOUR DB!!!
+
+To sum up things: I recommend you to give it a try and read about me in my github profile -> https://github.com/ArielZ123
+If you need anything feel free to ask me.

989ee5beaaad06de649f386e0286e3d86015ed3e
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2022-04-19 19:43:28 -0400

    Update fxmanifest.lua

--- scrapman-job/fxmanifest.lua
+++ scrapman-job/fxmanifest.lua
@@ -5,7 +5,7 @@ author 'ArielZ'
 description 'OverTrue RolePlay scrapman-job for esx, you can convert it if you want to other frameworks feel free to use the script but please dont sell the script.'
 version '1.0.0'
 
-shared_script 'main/config.lua
+shared_script 'main/config.lua'
 
 client_scripts {
 	'main/cl_scrapman.lua',

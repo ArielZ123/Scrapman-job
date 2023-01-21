9f34457c90111de3d54def0f3cd5289ea086f2ac
ArielZ <74502022+ArielZ123@users.noreply.github.com>
2023-01-21 19:30:04 +0200

    Update cl_scrapman.lua

--- scrapman-job/main/cl_scrapman.lua
+++ scrapman-job/main/cl_scrapman.lua
@@ -1,11 +1,13 @@
-ESX = nil
+if Config.OldEsx == true then
+   ESX = nil
 
-Citizen.CreateThread(function()
-	while ESX == nil do
+   Citizen.CreateThread(function()
+	 while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
-	end
-end)
+	 end
+   end)
+end
 
 local InJob = false
 local scrap_type = nil
